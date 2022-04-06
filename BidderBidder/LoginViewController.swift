f7d144dc43ef7c82676894eb22852ec7a3bdce5c
SungHyunC <rlatjdgus180@naver.com>
2022-04-06 10:04:34 +0900

    FEAT: BDBD-117 IOS 로그인 유형 선택 화면 만들기

---
+++ BidderBidder/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  BidderBidder
+//
+//  Created by 김성현 on 2022/04/06.
+//
+
+import UIKit
+
+class LoginViewController : UIViewController {
+    
+    
+    @IBOutlet weak var socialLoginButton1: UIButton!
+    
+    @IBOutlet weak var socialLoginButton2: UIButton!
+    
+    @IBOutlet weak var normalLoginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        [socialLoginButton1,socialLoginButton2,normalLoginButton].forEach {
+            $0?.layer.borderWidth = 1
+            $0?.layer.borderColor = UIColor.black.cgColor
+            $0?.layer.cornerRadius = 30
+        }
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //Navigation Bar 숨기기
+        navigationController?.navigationBar.isHidden = true
+    }
+}
