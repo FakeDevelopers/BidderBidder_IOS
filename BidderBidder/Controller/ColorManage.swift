d5cec2f9213599662dc1c0b046a41fc0830989c5
SungHyunC <rlatjdgus180@naver.com>
2022-05-23 19:27:57 +0900

    FEAT : 회원가입 화면 UI 제작

---
+++ BidderBidder/Controller/ColorManage.swift
@@ -0,0 +1,25 @@
+//
+//  ColorManage.swift
+//  BidderBidder
+//
+//  Created by 김성현 on 2022/05/18.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let skyBlueColor = UIColor(rgb: 0x21CEFF).cgColor
+    
+    convenience init(red: UInt8, green: UInt8, blue: UInt8) {
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: UInt8((rgb >> 16) & 0xFF),
+            green: UInt8((rgb >> 8) & 0xFF),
+            blue: UInt8(rgb & 0xFF)
+        )
+    }
+}
--- BidderBidder/LoginViewController.swift
+++ BidderBidder/LoginViewController.swift
@@ -6,19 +6,45 @@
 //
 
 import UIKit
+import TweeTextField
+import SwiftUI
 
 class LoginViewController : UIViewController {
     
-    
+    @IBOutlet weak var myPasswordTextField: TweeAttributedTextField!
     @IBOutlet weak var socialLoginButton1: UIButton!
-    
     @IBOutlet weak var socialLoginButton2: UIButton!
-    
     @IBOutlet weak var normalLoginButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    }
+    
+    
+    // 이게 없으면 터집니다.
+    @IBAction func passwordBeginEditing(_ sender: TweeAttributedTextField) {
+    }
+    
+    // 비밀번호 글자가 입력중 일 때 <임시>
+    @IBAction func passwordWhileEditing(_ sender: TweeAttributedTextField) {
+        if let userInput = sender.text {
+            if userInput.count == 0 {
+                sender.hideInfo(animated: true)
+            } else if userInput.count < 3 {
+                sender.infoTextColor = .red
+                sender.activeLineColor = .red
+                sender.showInfo("3글자 이상 입력하세요!", animated: true)
+            } else {
+                sender.infoTextColor = .green
+                sender.activeLineColor = .blue
+                sender.showInfo("잘 하셨습니다!", animated: true)
+            }
+        }
+    }
+    // 이게 없으면 터집니다.
+    @IBAction func passwordEndEditing(_ sender: TweeAttributedTextField) {
     }
     
     
@@ -28,26 +54,31 @@ class LoginViewController : UIViewController {
         //Navigation Bar 숨기기
         navigationController?.navigationBar.isHidden = true
     }
+    
 }
 
 
 class CornerButton: UIButton {
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         self.layer.borderWidth = 1
-        self.layer.borderColor = UIColor.black.cgColor
-        self.layer.cornerRadius = 30
+        self.layer.backgroundColor = UIColor.skyBlueColor
+        self.layer.borderColor = UIColor.skyBlueColor
+        self.layer.cornerRadius = 3
     }
+    
+    
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         
         self.layer.borderWidth = 1
-        self.layer.borderColor = UIColor.black.cgColor
-        self.layer.cornerRadius = 30
+        self.layer.backgroundColor = UIColor.skyBlueColor
+        self.layer.borderColor = UIColor.skyBlueColor
+        self.layer.cornerRadius = 25
     }
-
+    
 }
 
