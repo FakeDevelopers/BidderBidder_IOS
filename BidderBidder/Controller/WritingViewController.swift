fcfa89eabfd40998bda66ea020e3488636433063
Dorii0513 <bobodorii0513@gmail.com>
2022-07-14 21:45:41 +0900

    FIX: BDBD-333 URL 수정

--- BidderBidder/Controller/WritingViewController.swift
+++ BidderBidder/Controller/WritingViewController.swift
@@ -10,6 +10,10 @@ import Alamofire
 
 class WritingViewController: UIViewController, UITextViewDelegate{
     
+    struct Constant {
+        static let DomainURL:String = "http://3.38.81.213:8080"
+    }
+    
     @IBOutlet weak var productTitleTextField: UITextField!
     @IBOutlet weak var hopePriceTextField: UITextField!
     @IBOutlet weak var openingBidTextField: UITextField!
@@ -45,7 +49,7 @@ class WritingViewController: UIViewController, UITextViewDelegate{
             "productContent": productContent!
         ]
         
-        sendRestRequest(url: "http://3.38.81.213:8080/product/write", params: parameters as Dictionary) {
+        sendRestRequest(url: Constant.DomainURL + "/product/write", params: parameters as Dictionary) {
                         response in
                         switch response.result {
                             
