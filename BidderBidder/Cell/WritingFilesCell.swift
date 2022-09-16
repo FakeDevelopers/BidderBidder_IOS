b79db12b17548ffc00ffd8a4d979ec99922c9a46
Dorii0513 <bobodorii0513@gmail.com>
2022-09-17 04:37:33 +0900

    BDBD-421 화면 추가 수정

--- BidderBidder/Cell/WritingFilesCell.swift
+++ BidderBidder/Cell/WritingFilesCell.swift
@@ -17,8 +17,10 @@ class WritingFilesCell: UICollectionViewCell {
     
     override func awakeFromNib() {
         super .awakeFromNib()
+        
         self.files.layer.cornerRadius = 5
         self.files.clipsToBounds = true
+        
         self.representPictureTextView.clipsToBounds = true
         self.representPictureTextView.layer.cornerRadius = 5
         self.representPictureTextView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMaxYCorner, .layerMaxXMaxYCorner)
