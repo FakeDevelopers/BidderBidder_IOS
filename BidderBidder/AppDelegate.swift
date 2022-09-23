bfe54243cfc2c21179288514366e4616187da3eb
Dorii0513 <bobodorii0513@gmail.com>
2022-09-24 00:45:23 +0900

    FIX : BDBD-421 리뷰 수정

--- BidderBidder/AppDelegate.swift
+++ BidderBidder/AppDelegate.swift
@@ -9,7 +9,8 @@ import UIKit
 import FirebaseCore
 import FirebaseAuth
 import UserNotifications
- @main
+
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
